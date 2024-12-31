ee6d1a73498b4967c9c7b11525686e599b61062a
Gayatri <gayatridevi@gmail.com>
2024-12-31 21:20:17 +0530

    commit-1

---
+++ src/main/java/exam/lab/App.java
@@ -0,0 +1,14 @@
+package exam.lab;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "22BD1A6651" );
+    }
+}
